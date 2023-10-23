16672c3c82105c14f4e219fa141ba9218926dfe4
Victor Pimenta <victor.usdevcontact@gmail.com>
2023-10-23 19:35:18 -0400

    work on models

--- model/product.model.js
+++ model/product.model.js
@@ -1,7 +1,26 @@
 import { Schema, model } from "mongoose";
 
-const productSchema = new Schema(
+const productSchema = new Schema({
+  title: {
+    type: String,
+    enum: [
+      "Old Towne",
+      "Mega Olde Towne",
+      "Bullnose Coping",
+      "Remodel Bullnose Coping",
+      "4x8 Bricks",
+      "Turf Block",
+      "Travertine",
+      "Tile",
+    ],
+    required: true  },
+    description: { type: String, required: true },
+    color: { type: String, required:true },
+    thickness: { type: String, required:true },
+    dimensions: { type: String, required:true },
+    photo: { type: String, default: "https://www.tremron.com/images/products_page/ultra-combo.jpg" },
+},
+{ timestamps: true }
+);
 
-)
-
-export default Model("Product", productSchema);
+export default model("Product", productSchema);
--- model/user.model.js
+++ model/user.model.js
@@ -1,24 +1,75 @@
 import { Schema, model } from "mongoose";
 
+const phoneRegex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
 const userSchema = new Schema(
-   {
-      email: {
-         type: String,
-         required: true,
-         unique: true,
-         trim: true,
-         match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, // match = regex
+  {
+    name: { type: String, required: true, trim: true },
+    age: { type: Number, required: true, min: 18 },
+    telephone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    cpf: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: function (phone) {
+          return phoneRegex.test(phone);
+        },
+        message: "Invalid phone number",
       },
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-      profilePicture: {
-         type: String,
-         default: "https://cdn.wallpapersafari.com/92/63/wUq2AY.jpg",
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, // match = regex
+    },
+
+    profilePicture: {
+      type: String,
+      default: "https://cdn.wallpapersafari.com/92/63/wUq2AY.jpg",
+    },
+    history_inquiries: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Inquirie",
+        status: {
+          type: String,
+          enum: ["Disponivel", "Indisponivel"],
+          required: true,
+        },
       },
+    ],
+    history_projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        status: {
+          type: String,
+          enum: ["Disponivel", "Indisponivel"],
+          required: true,
+        },
+      },
+    ],
+    active: {
+      type: Boolean,
+      default: true,
+    },
 
-      passwordHash: { type: String, required: true },
-   },
-   // o que mais eu posso colocar nas opcoes do schema?
-   { timestamps: true }
+    passwordHash: { type: String, required: true },
+  },
+  // o que mais eu posso colocar nas opcoes do schema?
+  { timestamps: true }
 );
 
 export default model("User", userSchema);
